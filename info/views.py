b9bf1fcc11d378b768311f85ff9a94e5f3408a74
Ri-Dearg <sheridan.rp@gmail.com>
2020-11-26 23:10:06 +0000

    Added carousel to homepage
    Styles created, custom css added

--- info/views.py
+++ info/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import HomeCarousel
 
 
 # Create your views here.
 def render_index(request):
-    return render(request, 'info/index.html')
+    carousel = HomeCarousel.objects.all()
+    return render(request, 'info/index.html', {'carousel': carousel})
