fc6f87ac77b8dee682c2330a3fa1b7bd77bc77ba
Ri-Dearg <sheridan.rp@gmail.com>
2020-11-28 19:33:00 +0000

    Added script to adjust review carousel height

---
+++ static/js/custom/index.js
@@ -0,0 +1,32 @@
+function carouselNormalization() {
+    var items = $("#review-carousel .carousel-item"), //grab all slides
+        heights = [], //create empty array to store height values
+        tallest; //create variable to make note of the tallest slide
+    console.log(items)
+    var normalizeHeights = function () {
+
+        items.each(function () { //add heights to array
+            heights.push($(this).height());
+        });
+        tallest = Math.max.apply(null, heights); //cache largest value
+        items.each(function () {
+            $(this).css('min-height', tallest + 'px');
+            $(".carousel-row").css('min-height', tallest + 'px');
+        });
+    };
+
+    normalizeHeights();
+
+    $(window).on('resize orientationchange', function () {
+        //reset vars
+        tallest = 0;
+        heights.length = 0;
+
+        items.each(function () {
+            $(this).css('min-height', '0'); //reset min-height
+            $(".carousel-row").css('min-height', tallest + 'px');
+        });
+        normalizeHeights(); //run it again 
+    });
+
+};
