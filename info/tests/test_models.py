7fd433fd727851395adb5f3aa4712f7e5cc1d9f4
Ri-Dearg <sheridan.rp@gmail.com>
2020-11-29 22:29:37 +0000

    Added tests for review string.

--- info/tests/test_models.py
+++ info/tests/test_models.py
@@ -3,7 +3,7 @@ import re
 from django.test import TestCase
 from django.core.files.uploadedfile import SimpleUploadedFile
 
-from info.models import HomeCarousel, HomeInfo
+from info.models import HomeCarousel, HomeInfo, Review
 
 
 class ProductTests(TestCase):
@@ -12,12 +12,12 @@ class ProductTests(TestCase):
     def setUp(self):
         """Sets up a HomeCarousel model."""
         valid_carousel = HomeCarousel(name='HC1',
-                              description='description',
-                              image=SimpleUploadedFile(
-                                  name='default.jpg',
-                                  content=open(
-                                      'media/default.jpg', 'rb').read(),
-                                  content_type='image/jpeg',))
+                                      description='description',
+                                      image=SimpleUploadedFile(
+                                          name='default.jpg',
+                                          content=open(
+                                           'media/default.jpg', 'rb').read(),
+                                          content_type='image/jpeg',))
         valid_carousel.save()
 
         valid_info = HomeInfo(name='HI1',
@@ -30,6 +30,12 @@ class ProductTests(TestCase):
                               button_text="text here",
                               order=1)
         valid_info.save()
+
+        valid_review = Review(language='en_US',
+                              reviewer_name='abacus',
+                              text='this is a review')
+
+        valid_review.save()
 
     def test_carousel_image_file_is_processed_correctly(self):
         """Tests that an uploaded HomeCarousel image is resized and
@@ -52,7 +58,7 @@ class ProductTests(TestCase):
                                   new_info.image.name))
 
     def test_carousel_str(self):
-        """Tests the string method on the stockdrop."""
+        """Tests the string method on the HomeCarousel."""
         hc1 = HomeCarousel.objects.latest('date_added')
         self.assertEqual(str(hc1),
                          (f'{hc1.name}'))
@@ -78,7 +84,13 @@ class ProductTests(TestCase):
                                   new_info.image.name))
 
     def test_info_str(self):
-        """Tests the string method on the stockdrop."""
+        """Tests the string method on the HomeInfo."""
         hi1 = HomeInfo.objects.latest('date_added')
         self.assertEqual(str(hi1),
                          (f'{hi1.name}'))
+
+    def test_review_str(self):
+        """Tests the string method on the Review."""
+        r1 = Review.objects.latest('date_added')
+        self.assertEqual(str(r1),
+                         (f'{r1.reviewer_name}'))
