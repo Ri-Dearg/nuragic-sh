563522169091acea34e898c185428249c95245b9
Ri-Dearg <sheridan.rp@gmail.com>
2020-11-29 23:07:26 +0000

    EMails are now read-only on the email admin page.

--- contact/admin.py
+++ contact/admin.py
@@ -4,4 +4,10 @@ from django.contrib import admin
 
 from .models import Email
 
-admin.site.register(Email)
+
+class EmailAdmin(admin.ModelAdmin):
+    """Prevents emails from being editable on the admin page."""
+    readonly_fields = ('email', 'name', 'subject', 'date', 'message')
+
+
+admin.site.register(Email, EmailAdmin)
