66386f6e6f88c176244d7d39a4d76b76379569ae
Rory Patrick Sheridan <Ri-Dearg@users.noreply.github.com>
2020-12-25 13:19:37 +0100

    Updated tests

--- contact/tests/test_views.py
+++ contact/tests/test_views.py
@@ -1,12 +1,29 @@
-from django.test import TestCase
-
 from contact.models import Newsletter
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.test import TestCase
+from info.models import Category
 
 
 class TestContactViews(TestCase):
 
     def setUp(self):
+        image = SimpleUploadedFile(
+            name='default.jpg',
+            content=open(
+                'media/default.jpg',
+                 'rb').read(),
+            content_type='image/jpeg',)
         Newsletter.objects.create(name="basic")
+        valid_category = Category(title_en='about',
+                                  title_it='about',
+                                  menu_word_en='HCMW1',
+                                  menu_word_it='HCMW1',
+                                  description_en='description',
+                                  description_it='description',
+                                  image=image,
+                                  button_text="text here",
+                                  order=1)
+        valid_category.save()
 
     """Tests views for the Email app."""
     # Confirms the correct template is used
@@ -16,9 +33,11 @@ class TestContactViews(TestCase):
         self.assertTemplateUsed('contact_form.html')
 
     def test_newsletter_signup(self):
-        self.client.post('/contact/f/newsletter/', {'email_en': 'test@test.com'},
+        self.client.post('/contact/f/newsletter/',
+                         {'email_en': 'test@test.com'},
                          HTTP_X_REQUESTED_WITH='XMLHttpRequest')
-        self.client.post('/it/contact/f/newsletter/', {'email_it': 'test@test.com'},
+        self.client.post('/it/contact/f/newsletter/',
+                         {'email_it': 'test@test.com'},
                          HTTP_X_REQUESTED_WITH='XMLHttpRequest')
 
         newsletter = Newsletter.objects.get(name="basic")
