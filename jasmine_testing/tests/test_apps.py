0677af1e1cda684bff665fea95dd28ee8bbaf254
Rory Patrick Sheridan <Ri-Dearg@users.noreply.github.com>
2021-01-01 20:05:01 +0100

    Jasmine app commented.

--- jasmine_testing/tests/test_apps.py
+++ jasmine_testing/tests/test_apps.py
@@ -1,11 +1,14 @@
+"""Tests App Config."""
 from django.apps import apps
 from django.test import TestCase
 from jasmine_testing.apps import JasmineTestingConfig
 
 
 class TestJasmineConfig(TestCase):
+    """Tests the Jasmine app config."""
 
     def test_app(self):
+        """Tests App name"""
         self.assertEqual('jasmine_testing', JasmineTestingConfig.name)
         self.assertEqual('jasmine_testing',
                          apps.get_app_config('jasmine_testing').name)
