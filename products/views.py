2f3b75ad8ede98fb9f43b023eeef3a57ba6864fd
Rory Patrick Sheridan <sheridan.rp@gmail.com>
2021-01-15 17:15:10 +0100

    Ui Updates for clearer titles, active menu items

--- products/views.py
+++ products/views.py
@@ -5,15 +5,24 @@ from .models import Product, ShopCategory
 
 
 class ShopCategoryDetailView(DetailView, MultipleObjectMixin):
+    """Displays a list of products in the Category.
+    The MultipleObjectMixin allows for easy pagination."""
     model = ShopCategory
     paginate_by = 4
 
     def get_context_data(self, **kwargs):
         """Adds all necessary information to the context"""
+
+        # Declares objects for pagination
         object_list = self.object.products.all().order_by(
             '-stock', '-popularity')
         context = super(ShopCategoryDetailView, self).get_context_data(
             object_list=object_list, **kwargs)
+
+        this_object = context['object']
+        # Selects the active tab
+        if f'/category/{this_object.id}' in self.request.path:
+            context['active_category'] = f'{this_object.id}'
 
         return context
 
