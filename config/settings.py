efa99e13f2e58bfc9bd100e1a25dcf855accd8b8
Rory Patrick Sheridan <sheridan.rp@gmail.com>
2021-01-26 13:29:42 +0100

    Allauth basic settings added

--- config/settings.py
+++ config/settings.py
@@ -261,3 +261,8 @@ AUTHENTICATION_BACKENDS = [
     # `allauth` specific authentication methods, such as login by e-mail
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
+
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_USERNAME_MIN_LENGTH = 4
