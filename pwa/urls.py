a002062df3d5c0ed98deb03b6ea1a7429c3802ae
Rory Patrick Sheridan <sheridan.rp@gmail.com>
2021-02-27 23:59:04 +0100

    offline page added to pwa

--- pwa/urls.py
+++ pwa/urls.py
@@ -1,13 +1,12 @@
 """Urls for the pwa module."""
 from django.urls import path
 
-from .views import SWTemplateView
+from .views import SWTemplateView, render_offline
 
 urlpatterns = [
     # Service Worker Template string
-    path(
-        'sw.js',
-        SWTemplateView.as_view(),
-        name='service-worker',
-    ),
+    path('sw.js',
+         SWTemplateView.as_view(), name='service-worker'),
+    path('offline/',
+         render_offline, name='offline'),
 ]
--- pwa/views.py
+++ pwa/views.py
@@ -1,7 +1,13 @@
 """Views for the PWA app."""
+from django.shortcuts import render
 from django.views.generic import TemplateView
 
 from config import version
+
+
+def render_offline(request):
+    """Renders a page explaining the app is offline."""
+    return render(request, 'pwa/offline.html')
 
 
 class SWTemplateView(TemplateView):
