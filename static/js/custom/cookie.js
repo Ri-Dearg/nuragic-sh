485f5fc0006e6bda00b127ef11a94b83a7f7e340
Rory Patrick Sheridan <sheridan.rp@gmail.com>
2021-05-12 16:24:00 +0200

    Corrected modal animations.

--- static/js/custom/cookie.js
+++ static/js/custom/cookie.js
@@ -1,21 +1,16 @@
-var controller = new AbortController();
-var signal = controller.signal;
-
 var cookieOffcanvas = document.getElementById("offcanvasCookies");
 var bsCookieOffcanvas = new bootstrap.Offcanvas(cookieOffcanvas);
 
-cookieOffcanvas.addEventListener("show.bs.offcanvas", function () {
-  document.body.addEventListener(
-    "click",
-    function (e) {
-      e.stopPropagation();
-    },
-    { signal: signal }
-  );
+cookieOffcanvas.addEventListener("shown.bs.offcanvas", function () {
+  var bsModal = document.getElementsByClassName("modal-backdrop")[0];
+  var myModal = bsModal.cloneNode(true);
+  bsModal.parentNode.replaceChild(myModal, bsModal);
 });
+
+bsCookieOffcanvas.show();
+
 document.querySelectorAll(".cookie-form").forEach((item) => {
   item.addEventListener("submit", function () {
     bsCookieOffcanvas.hide();
-    controller.abort();
   });
 });
