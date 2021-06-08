f7bd5e9f107d5dff690dcb9e00f58360b4bf5f9c
Rory Patrick Sheridan <sheridan.rp@gmail.com>
2021-06-08 15:01:48 +0200

    Updated cookie consent to keep tags on all pages

--- cookies/urls.py
+++ cookies/urls.py
@@ -1,8 +1,9 @@
 """Urls for cookie app."""
 from django.urls import path
 
-from .views import cookie_consent
+from .views import cookie_consent, update_cookies
 
 urlpatterns = [
     path('consent/', cookie_consent, name='consent'),
+    path('update/', update_cookies, name='update'),
 ]
--- static/js/custom/cookie.js
+++ static/js/custom/cookie.js
@@ -1,23 +1,6 @@
 var cookieOffcanvas = document.getElementById("offcanvasCookies");
 var bsCookieOffcanvas = new bootstrap.Offcanvas(cookieOffcanvas);
 
-function injectTracker(scriptUrl) {
-  function gAnalyticsLoaded(scriptUrl) {
-    let moreScript = document.createElement("script");
-    moreScript.setAttribute("src", scriptUrl);
-    document.head.appendChild(moreScript);
-  }
-
-  let gAnalytics = document.createElement("script");
-  gAnalytics.setAttribute(
-    "src",
-    "https://www.googletagmanager.com/gtag/js?id=G-TY5DN8WWYB"
-  );
-  document.head.appendChild(gAnalytics);
-
-  gAnalytics.addEventListener("load", gAnalyticsLoaded(scriptUrl), false);
-}
-
 function cookieForm(object) {
   // The data sent in the form POST request.
   const formData = new FormData(object);
@@ -38,7 +21,9 @@ function cookieForm(object) {
     })
     .then((data) => {
       if (data.consent === "true") {
-        injectTracker(data.script);
+        $.get(data.url, function (page) {
+          $(page).appendTo("head");
+        });
       }
       toastMessage(data.tag, data.tagMessage, data.message);
     })
