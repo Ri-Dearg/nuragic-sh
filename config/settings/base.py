531172b5360875cee76521ce619deb25cebc36fb
Rory Patrick Sheridan <sheridan.rp@gmail.com>
2021-06-18 16:08:18 +0200

    Bugifx bump

--- config/settings/base.py
+++ config/settings/base.py
@@ -24,7 +24,7 @@ except ModuleNotFoundError:
     pass
 
 # Version Number
-VERSION = '0.7.3'
+VERSION = '0.7.4'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parents[2]
